302f154db99984da572c8b5057a6b57740d725f6
ThunderStorm710 <pascensao77@gmail.com>
2024-05-13 20:35:22 +0100

    Base de dados Postgres AWS feita

--- backend/clinicaSaude/clinicaSaude/settings.py
+++ backend/clinicaSaude/clinicaSaude/settings.py
@@ -80,11 +80,22 @@ WSGI_APPLICATION = 'clinicaSaude.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "bacnekdaws",
+        'USER': "postgres",
+        'PASSWORD': "postgres",
+        'HOST': "bacnekdaws.cu2cua8ubnyk.us-east-1.rds.amazonaws.com",
+        'PORT': 5432,
     }
 }
 
