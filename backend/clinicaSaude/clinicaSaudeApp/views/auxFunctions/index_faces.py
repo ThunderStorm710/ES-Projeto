ac85428b1e645c5a73e5122d9ce75d730531d4c5
ThunderStorm710 <pascensao77@gmail.com>
2024-05-17 15:11:13 +0100

    Inicio State Machine e Face ID

--- backend/clinicaSaude/clinicaSaudeApp/views/auxFunctions/index_faces.py
+++ backend/clinicaSaude/clinicaSaudeApp/views/auxFunctions/index_faces.py
@@ -1,8 +1,8 @@
 import boto3
 
-aws_access_key_id="ASIA453LFNKUMFQ4VOTB"
-aws_secret_access_key="VSLvl4tNppyfX8av19DXrEFUKcjYAyVjGm3oC3EL"
-aws_session_token="IQoJb3JpZ2luX2VjEFMaCXVzLXdlc3QtMiJGMEQCIHnflo7Zro5jgZ3R4pv9P1+MNrJiuLuZ1lmrG0JqksiOAiB7UhhggfVzRRU/mA2zPBN7a3Ml8czNjOFHvmbmBhjBGyq9Agi8//////////8BEAAaDDg4ODc0NjEwOTYwOCIMiFVVefRdY3Ir2bCpKpECttddykVcVKiNjrV7bGxtB9x6SGkbP6OvprqjlhIBV3rMLZ1xh/MMrKquod/lPou6Mm25yPAONFMlP90JSiBaXMoTVMGJm+oXWbRELnRyKUBESnD2YK4j2bxPD95P2OL+qshr7PM94GsXqjo8KnbtLs1kKNMPqP0cyTNe6JSb/XVUQCB30/1ydUk3fz9lDJjzxnlgmtzN/v7QAz79awuWtN3W0uxkXRtssoSCTJTLO28wzjy/DijusnkZYIqW1S4ipmkoN2ZheE/sQUDSpLfQTrOhLxvyrCXCJisYLeZPMB3MN6EPY+eSo7vgXB+BoJBmHUeHhAeNRirQtjPFcfnkzcXD4HMY41nv7lYaCv5ClrqHMMnIl7IGOp4Bb/8QI+seFObP9D8OeLX8QMnp9sXHgAQiIdgN8IosyLpHZtFJePAWu06T7mFiMYOyL3bL2G5TaThQJ88jGgWhtN2yhB7aDmMX/7zexXOmnKw1vZ/n5Si5dHSvAvvEx01hyQopwcvf2BO70qApt8lgk/FOU/11Vbikz066OBhUDblPC+P1LveyWh3unHBV0iAHr3VpdshmPVlh6agIYOY="
+aws_access_key_id=""
+aws_secret_access_key=""
+aws_session_token=""
 s3 = boto3.resource('s3', region_name='us-east-1', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
                     aws_session_token=aws_session_token)
 
